75d2b230506a547bef42db45825c167fc2df68b6
“Diego <diegonacimiento18@gmail.com>
2022-12-01 17:45:12 -0300

    app terminada

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // Agregamos la propiedad base y como valor ponemos el nombre de nuestro repositorio:
+  base: "/tasking-app/"
 })
