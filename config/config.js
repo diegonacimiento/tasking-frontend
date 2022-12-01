fefd13e6d8a4a85b0c6fc87985bd2754b55d82a4
“Diego <diegonacimiento18@gmail.com>
2022-12-01 18:01:59 -0300

    tasking terminado

--- config/config.js
+++
@@ -1,9 +0,0 @@
-// import dotenv from "dotenv";
-// dotenv.config({
-//     path: "../.env"
-// });
-// const config = {
-//   db_Url: process.env.URL,
-// };
-// export { config };
-// console.log(config.db_Url)
