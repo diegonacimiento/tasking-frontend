f04b78d9dc52e7f7176cf4291022c029e38d4655
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-08-01 12:13:10 -0300

    actualizacion boton borrar busqueda

--- src/components/tasks/tasks-components/searchTasks/SearchTasks.jsx
+++ src/components/tasks/tasks-components/searchTasks/SearchTasks.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { VscClose } from "react-icons/vsc";
 import "./searchTasks.css";
 
 export default function SearchTasks({ tasks, setResultSearch }) {
+  const buttonDelete = useRef(null);
+  const [valueInput, setValueInput] = useState("");
 
   function searchTask(text) {
+    buttonDelete.current.style.display = "inline-block";
     const result = tasks.filter((task) => {
       return task.description.includes(text.toLowerCase());
     });
     result.length > 0 ? setResultSearch([...result]) : setResultSearch(undefined);
-    if (text === "") {
+    if (!text) {
       setResultSearch(null);
+      buttonDelete.current.style.display = "none";
+      return;
     }
   }
 
   function search(e) {
+    setValueInput(e.target.value);
     searchTask(e.target.value);
+  }
+
+  function deleteSearch() {
+    setValueInput("");
+    searchTask("");
   }
 
   return (
     <div className="search-tasks-div">
-      <input onChange={search} placeholder="Buscar" type={"text"} />
+      <input onChange={search} placeholder="Buscar" type="text" value={valueInput} autoComplete="off" />
+      <button title="Borrar búsqueda" onClick={deleteSearch} ref={buttonDelete}><VscClose /></button>
     </div>
   );
 }
