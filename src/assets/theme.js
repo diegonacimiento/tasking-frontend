1f4529d3ae20e9861fa52cd554ea0a70f5993b1a
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-09-14 17:17:28 -0300

    feat: added theme user default

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -1,25 +1,30 @@
 const style = document.documentElement.style;
 
 function toggleTheme(isDarkMode) {
-    if (isDarkMode) {
-        style.setProperty("--colorRoot", "rgb(34, 34, 34)");
-        style.setProperty("--colorBotton", "rgb(255, 255, 255)");
-        style.setProperty("--bkButton", "#343434");
-        style.setProperty("--loading-top", "#bde0fe");
-        style.setProperty(
-            "--image",
-            'url("https://i.ibb.co/kQZ260V/tasking-Blanco.png")'
-        )
-    } else {
-        style.setProperty("--colorRoot", "rgb(230, 230, 230)");
-        style.setProperty("--colorBotton", "rgb(0, 0, 0)");
-        style.setProperty("--bkButton", "#adb5bd");
-        style.setProperty("--loading-top", "#03045e");
-        style.setProperty(
-            "--image",
-            'url("https://i.ibb.co/h89BLcF/tasking.png")'
-        )
-    }
+  if (isDarkMode) {
+    darkMode();
+  } else {
+    lightMode();
+  }
 }
 
 export default toggleTheme;
+
+const darkMode = () => {
+  style.setProperty("--colorRoot", "rgb(34, 34, 34)");
+  style.setProperty("--colorBotton", "rgb(255, 255, 255)");
+  style.setProperty("--bkButton", "#343434");
+  style.setProperty("--loading-top", "#bde0fe");
+  style.setProperty(
+    "--image",
+    'url("https://i.ibb.co/kQZ260V/tasking-Blanco.png")'
+  );
+};
+
+const lightMode = () => {
+  style.setProperty("--colorRoot", "rgb(230, 230, 230)");
+  style.setProperty("--colorBotton", "rgb(0, 0, 0)");
+  style.setProperty("--bkButton", "#adb5bd");
+  style.setProperty("--loading-top", "#03045e");
+  style.setProperty("--image", 'url("https://i.ibb.co/h89BLcF/tasking.png")');
+};
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,11 @@ import "./header.css";
 
 const isMobile = window.innerWidth <= 712 || window.innerHeight <= 575;
 
-const isDarkModeParse = JSON.parse(localStorage.getItem("isDarkMode"));
+const themeUser =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const isDarkModeParse = JSON.parse(localStorage.getItem("isDarkMode")) ?? themeUser;
 
 export default function Header() {
   const navigate = useNavigate();
@@ -28,7 +32,7 @@ export default function Header() {
 
   useEffect(() => {
     toggleTheme(isDarkMode);
-  }, [])
+  }, []);
 
   function changeMode() {
     isDarkMode
@@ -40,28 +44,29 @@ export default function Header() {
         toggleTheme(true));
   }
 
-  addEventListener('resize', () => {
-    const newIsMobileScreen = window.innerWidth <= 712 || window.innerHeight <= 575; 
+  addEventListener("resize", () => {
+    const newIsMobileScreen =
+      window.innerWidth <= 712 || window.innerHeight <= 575;
     setIsMobileScreen(newIsMobileScreen);
-  })
+  });
 
   return (
     <header>
-      {(token && isMobileScreen) && (
-        <HeaderMobile logout={logout} />
-      )
-      }
+      {token && isMobileScreen && <HeaderMobile logout={logout} />}
 
-      <div onClick={() => window.location = "/"} className="logo-contain">
+      <div onClick={() => (window.location = "/")} className="logo-contain">
         <span className="image"></span>
         <p className="letra">Tasking</p>
       </div>
 
-      {(token && !isMobileScreen) && (
-        <HeaderDesktop logout={logout} />
-      )}
+      {token && !isMobileScreen && <HeaderDesktop logout={logout} />}
 
-      <button title="Cambiar tema" className="mode" onClick={changeMode} type="button">
+      <button
+        title="Cambiar tema"
+        className="mode"
+        onClick={changeMode}
+        type="button"
+      >
         {isDarkMode ? <MdDarkMode /> : <MdLightMode />}
       </button>
     </header>
